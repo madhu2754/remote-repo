6a5c53374aec3824dccbe81b3d538f1e8599e01b
test <test@DESKTOP-110K89D>
2018-01-19 16:01:26 +0530

    second change1

--- Sel_learn/src/Sample.java
+++ Sel_learn/src/Sample.java
@@ -8,7 +8,7 @@ public class Sample {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\test\\Documents\\selenium_nov_new\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("www.google.com");
-		
+		System.out.println("Its working");
 		
 	}
 
